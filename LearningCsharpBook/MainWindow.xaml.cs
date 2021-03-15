4c5ce74de74dfd357b39e4871a9b331f7a4dedfd
urban.flame <urban.fl4m3@gmail.com>
2021-03-15 23:25:28 +0300

    Added int and string operations, text block and button

--- LearningCsharpBook/MainWindow.xaml.cs
+++ LearningCsharpBook/MainWindow.xaml.cs
@@ -24,5 +24,33 @@ namespace LearningCsharpBook
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int number = 15;
+            number = number + 10;
+            number = 36 * 15;
+            number = 12 - (42 / 7);
+            number += 10;
+            number *= 3;
+            number = 71 / 3;
+
+            int count = 0;
+            count++;
+            count--;
+
+            ++count;
+            --count;
+
+            string result = "hello";
+            result += $" again {result}";
+            output.Text = result;
+            result = $"the value is: {count}";
+            result = string.Empty;
+
+            var yesNo = false;
+            var anotherBool = true;
+            yesNo = !anotherBool;
+        }
     }
 }
