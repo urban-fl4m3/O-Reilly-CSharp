3fcafd24f58822ad77ca561b95614fd19cb15b52
urban.flame <urban.fl4m3@gmail.com>
2021-03-16 00:52:07 +0300

    Removed unused libs

--- Chapter2_Program2/MainWindow.xaml.cs
+++ Chapter2_Program2/MainWindow.xaml.cs
@@ -3,9 +3,6 @@ using System.Windows;
 
 namespace Chapter2_Program2
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
