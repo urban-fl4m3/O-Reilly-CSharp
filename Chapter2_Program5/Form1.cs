6941e4545a86fc8396cc09b73adda8c9d26de4b7
urban.flame <urban.fl4m3@gmail.com>
2021-03-16 09:44:24 +0300

    Added logic for flashy thing

---
+++ Chapter2_Program5/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Chapter2_Program5
+{
+    public partial class FlashyThing : Form
+    {
+        public FlashyThing()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            while (Visible)
+            {
+                int c = 0;
+
+                while (c < 254 && Visible)
+                {
+                    BackColor = Color.FromArgb(c, 255 - c, c);
+                    Application.DoEvents();
+                    Thread.Sleep(3);
+                    c++;
+                }
+
+                while (c > -1 && Visible)
+                {
+                    BackColor = Color.FromArgb(c, 255 - c, c);
+                    Application.DoEvents();
+                    Thread.Sleep(3);
+                    c--;
+                }
+            }
+        }
+    }
+}
