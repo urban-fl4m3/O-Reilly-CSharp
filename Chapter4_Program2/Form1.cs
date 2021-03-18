433b6b1fec50a6e929cacf1be7ffdd5c0915eb2d
urban.flame <urban.fl4m3@gmail.com>
2021-03-19 02:46:59 +0300

    Added Chapter4_Program2 (Miles calculator)

---
+++ Chapter4_Program2/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Chapter4_Program2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            startingMileage = (int)numericUpDown1.Value;
+            endingMileage = (int)numericUpDown2.Value;
+
+            if (startingMileage < endingMileage)
+            {
+                milesTraveled = endingMileage -= startingMileage;
+                amountOwned = milesTraveled * reimburseRate;
+                label4.Text = $"${amountOwned}";
+            }
+            else
+            {
+                MessageBox.Show("Начальный пробег не может превышать конечный", "Cannot Calculate");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"{milesTraveled} miles");
+        }
+    }
+}
