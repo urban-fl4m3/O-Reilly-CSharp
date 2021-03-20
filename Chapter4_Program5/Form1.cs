98dd8654319f557e69e6604d31f96312aa61fb61
urban.flame <urban.fl4m3@gmail.com>
2021-03-20 16:56:54 +0300

    Added Chapter4_Program5 (Input trainer)

---
+++ Chapter4_Program5/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Chapter4_Program5
+{
+    public partial class Form1 : Form
+    {
+        private Random random = new Random();
+        private Stats stats = new Stats();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            listBox1.Items.Add((Keys)random.Next(65, 90));
+            if (listBox1.Items.Count > 7)
+            {
+                listBox1.Items.Clear();
+                listBox1.Items.Add("Игра окончена!");
+                timer1.Stop();
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (listBox1.Items.Contains(e.KeyCode))
+            {
+                listBox1.Items.Remove(e.KeyCode);
+                listBox1.Refresh();
+                if (timer1.Interval > 400)
+                    timer1.Interval -= 100;
+                if (timer1.Interval > 250)
+                    timer1.Interval -= 7;
+                if (timer1.Interval > 100)
+                    timer1.Interval -= 2;
+
+                difficultyProgressBar.Value = 800 - timer1.Interval;
+                stats.Update(true);
+            }
+            else
+            {
+                stats.Update(false);
+            }
+
+            correctLabel.Text = $"Correct: {stats.Correct}";
+            missedLabel.Text = $"Missed: {stats.Missed}";
+            totalLabel.Text = $"Total: {stats.Total}";
+            accuracyLabel.Text = $"Accuracy: {stats.Accuracy}%";
+        }
+    }
+}
