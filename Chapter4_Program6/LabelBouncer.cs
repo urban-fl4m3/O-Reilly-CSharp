29a409c13636a343462b4777cde762d18cfe204a
urban.flame <urban.fl4m3@gmail.com>
2021-03-20 17:05:48 +0300

    Added Chapter4_Program6 (Label bouncers)

---
+++ Chapter4_Program6/LabelBouncer.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows.Forms;
+
+namespace Chapter4_Program6
+{
+    class LabelBouncer
+    {
+        public Label MyLabel;
+
+        public bool MovingForward = true;
+
+        public void Move()
+        {
+            if (MyLabel != null)
+            {
+                if (MovingForward)
+                {
+                    MyLabel.Left += 5;
+
+                    if (MyLabel.Left >= MyLabel.Parent.Width - MyLabel.Width)
+                    {
+                        MovingForward = false;
+                    }
+                }
+                else
+                {
+                    MyLabel.Left -= 5;
+
+                    if (MyLabel.Left <= 0)
+                    {
+                        MovingForward = true;
+                    }
+                }
+            }
+        }
+    }
+}
