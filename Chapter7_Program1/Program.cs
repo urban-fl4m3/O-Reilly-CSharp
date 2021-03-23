95c6460a5ce5da8704452a1b22aefc6f8fca16e3
urban.flame <urban.fl4m3@gmail.com>
2021-03-24 00:04:19 +0300

    Added Chapter7_Program1 (TallGuy and Clown interface)

---
+++ Chapter7_Program1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Chapter7_Program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TallGuy tallGuy = new TallGuy() { Height = 74, Name = "Jimmy" };
+            tallGuy.TalkAboutYourself();
+        }
+    }
+}
---
+++ Chapter7_Program1/TallGuy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Chapter7_Program1
+{
+    class TallGuy
+    {
+        public string Name;
+        public int Height;
+
+        public void TalkAboutYourself()
+        {
+            Console.WriteLine($"My name is {Name} and I'm {Height} inches tall.");
+        }
+    }
+}
