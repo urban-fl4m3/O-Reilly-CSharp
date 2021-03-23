cdf7a131984193c7b213ad5dcf73620ac5776cb6
urban.flame <urban.fl4m3@gmail.com>
2021-03-23 14:46:36 +0300

    Added Chapter6_Program2 (Base and sub classes)

---
+++ Chapter6_Program2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Chapter6_Program2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            MySubClass sub = new MySubClass("abc", 2);
+           
+        }
+    }
+
+    class MyBaseClass
+    {
+        public MyBaseClass(string baseClassNeedThis)
+        {
+            Console.WriteLine($"This is a base class: {baseClassNeedThis}");
+        }
+    }
+
+    class MySubClass : MyBaseClass
+    {
+        public MySubClass(string baseClassNeedThis, int anotherValue) : base(baseClassNeedThis)
+        {
+            Console.WriteLine($"This is the subclass: {baseClassNeedThis} and {anotherValue}");
+        }
+    }
+}
