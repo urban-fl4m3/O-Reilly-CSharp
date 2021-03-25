93c95f5afee0bb0ca145722aede568ea4f651d57
urban.flame <urban.fl4m3@gmail.com>
2021-03-26 00:37:42 +0300

    Added Chapter8_Program2 (List methods)

---
+++ Chapter8_Program2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Chapter8_Program2
+{
+    static class Program
+    {
+        static void Main()
+        {
+            List<Shoe> shoeCloset = new List<Shoe>();
+
+            shoeCloset.Add(new Shoe()
+            { Style = Style.Sneakers, Color = "Чёрный" });
+            shoeCloset.Add(new Shoe()
+            { Style = Style.Clogs, Color = "Коричневый" });
+            shoeCloset.Add(new Shoe()
+            { Style = Style.Wingtips, Color = "Чёрный" });
+            shoeCloset.Add(new Shoe()
+            { Style = Style.Loafers, Color = "Белый" });
+            shoeCloset.Add(new Shoe()
+            { Style = Style.Loafers, Color = "Красный" });
+            shoeCloset.Add(new Shoe()
+            { Style = Style.Sneakers, Color = "Зелёный" });
+
+            int numberOfShoes = shoeCloset.Count;
+
+            foreach (Shoe shoe in shoeCloset)
+            {
+                shoe.Style = Style.Flipflops;
+                shoe.Color = "Оранжевый";
+            }
+
+            shoeCloset.RemoveAt(4);
+
+            Shoe thirdShoe = shoeCloset[3];
+            Shoe secondShoe = shoeCloset[2];
+            shoeCloset.Clear();
+
+            shoeCloset.Add(thirdShoe);
+
+            if (shoeCloset.Contains(secondShoe))
+            {
+                Console.WriteLine("Удвитиельно!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Chapter8_Program2/Shoe.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter8_Program2
+{
+    class Shoe
+    {
+        public Style Style;
+        public string Color;
+    }
+}
