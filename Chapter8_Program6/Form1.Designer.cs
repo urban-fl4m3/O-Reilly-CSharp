e75739d3f0a959593ebbdc3e8d37927762fc28eb
urban.flame <urban.fl4m3@gmail.com>
2021-04-01 18:31:39 +0300

    Added Chapter8_Program6 (Dictionary with baseball players)

---
+++ Chapter8_Program6/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace Chapter8_Program6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.number = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.playerNameLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.yearLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number";
+            // 
+            // number
+            // 
+            this.number.FormattingEnabled = true;
+            this.number.Location = new System.Drawing.Point(86, 12);
+            this.number.Name = "number";
+            this.number.Size = new System.Drawing.Size(65, 24);
+            this.number.TabIndex = 1;
+            this.number.SelectedIndexChanged += new System.EventHandler(this.number_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(167, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "was worn by";
+            // 
+            // playerNameLabel
+            // 
+            this.playerNameLabel.AutoSize = true;
+            this.playerNameLabel.Location = new System.Drawing.Point(258, 13);
+            this.playerNameLabel.Name = "playerNameLabel";
+            this.playerNameLabel.Size = new System.Drawing.Size(46, 17);
+            this.playerNameLabel.TabIndex = 3;
+            this.playerNameLabel.Text = "label3";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(350, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "and retired in";
+            // 
+            // yearLabel
+            // 
+            this.yearLabel.AutoSize = true;
+            this.yearLabel.Location = new System.Drawing.Point(449, 13);
+            this.yearLabel.Name = "yearLabel";
+            this.yearLabel.Size = new System.Drawing.Size(46, 17);
+            this.yearLabel.TabIndex = 5;
+            this.yearLabel.Text = "label4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 49);
+            this.Controls.Add(this.yearLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.playerNameLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.number);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox number;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label playerNameLabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label yearLabel;
+    }
+}
+
---
+++ Chapter8_Program6/JerseyNumber.cs
@@ -0,0 +1,14 @@
+﻿namespace Chapter8_Program6
+{
+    class JerseyNumber
+    {
+        public string Player { get; private set; }
+        public int YearRetired { get; private set; }
+
+        public JerseyNumber(string player, int numberRetired)
+        {
+            Player = player;
+            YearRetired = numberRetired;
+        }
+    }
+}
