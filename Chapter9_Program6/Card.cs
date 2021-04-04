76afbbc9af24c20a468ae7dd1e12329800d9fdfe
urban.flame <urban.fl4m3@gmail.com>
2021-04-04 11:50:57 +0300

    Added Chapter9_Program6 (Cards byte serializing)

---
+++ Chapter9_Program6/Card.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Chapter9_Program6
+{
+    enum Suits
+    {
+        Spades = 0,
+        Clubs = 1,
+        Diamonds = 2,
+        Hearts = 3
+    }
+
+    enum Values
+    {
+        Ace = 1,
+        Two = 2,
+        Three = 3,
+        Four = 4,
+        Five = 5,
+        Six = 6,
+        Seven = 7,
+        Eight = 8,
+        Nine = 9,
+        Ten = 10,
+        Jack = 11,
+        Queen = 12,
+        King = 13
+    }
+
+    [Serializable]
+    partial class Card
+    {
+        public Suits Suit { get; }
+        public Values Value { get; }
+        public string Name
+        {
+            get
+            {
+                return $"{Value} of {Suit}";
+            }
+        }
+
+        public Card(Suits suit, Values value)
+        {
+            Suit = suit;
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ Chapter9_Program6/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Chapter9_Program6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream output = File.Create("three-c.dat"))
+            using (BinaryWriter writer = new BinaryWriter(output))
+            {
+                Card card = new Card(Suits.Clubs, Values.Three);
+                BinaryFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(output, card);
+            }
+
+            using (FileStream output = File.Create("six-h.dat"))
+            using (BinaryWriter writer = new BinaryWriter(output))
+            {
+                Card card = new Card(Suits.Hearts, Values.Six);
+                BinaryFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(output, card);
+            }
+
+            byte[] firstFile = File.ReadAllBytes("three-c.dat");
+            byte[] secondFile = File.ReadAllBytes("six-h.dat");
+
+            for (int i = 0; i < firstFile.Length; i++)
+            {
+                if (firstFile[i] != secondFile[i])
+                {
+                    Console.WriteLine($"Byte #{i}: {firstFile[i]} versus {secondFile[i]}");
+                }
+            }
+        }
+    }
+}
