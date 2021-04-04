fe53e643e583fce6be9f2806772b4ef2c2e4f42d
urban.flame <urban.fl4m3@gmail.com>
2021-04-04 10:55:48 +0300

    Added Chapter9_Program4 (Eureka encoding)

---
+++ Chapter9_Program4/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chapter9_Program4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private const string eurekaName = "eureka.txt";
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText(eurekaName, "Eureka!");
+            byte[] eurekaBytes = File.ReadAllBytes(eurekaName);
+
+            foreach (byte b in eurekaBytes)
+            {
+                Console.Write($"{b} ");
+            }
+            Console.WriteLine();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText(eurekaName, "Eureka!");
+            byte[] eurekaBytes = File.ReadAllBytes(eurekaName);
+
+            foreach (byte b in eurekaBytes)
+            {
+                Console.Write($"{b:x2} ");
+            }
+            Console.WriteLine();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText(eurekaName, "שלום", Encoding.Unicode);
+            byte[] eurekaBytes = File.ReadAllBytes(eurekaName);
+
+            foreach (byte b in eurekaBytes)
+            {
+                Console.Write($"{b:x2} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
